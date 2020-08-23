cec1f1d1a7a0c71dc3c0da0aeaf486532e48d7d2
pilhwanKim <pilhwankim03@gmail.com>
2020-08-23 15:58:18 +0900

    AOP

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,17 +1,17 @@
 package hello.hellospring;
 
 //import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.JpaMemberRepository;
+//import hello.hellospring.repository.JdbcTemplateMemberRepository;
+//import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 //import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
+//import javax.persistence.EntityManager;
+//import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
